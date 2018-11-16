89867eafa188110bdc8c1338304c6fd3bd8bb6a6
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2018-11-16 12:04:00 +0900

    微修正

--- KoyominJ.py
+++ KoyominJ.py
@@ -6,7 +6,6 @@ app = wx.App(False)
 M_frame = menuWindow.Menu(None,"Menu")
 M_frame.Show()
 app.MainLoop()
-print("hello")
 Mode = menuWindow.Menu.Get_Mode_Value(M_frame)
 if Mode is not None:
     VN_frame = mainWindow.VN_window(None,"TEST",Mode)
